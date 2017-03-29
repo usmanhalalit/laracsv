2061d565ad951f6b23f2ea6dea9c43d41fce491f
abdulla malik <abdulla.coder@gmail.com>
2017-03-29 17:14:53 +0600

    Readme.md file edited and some space and indentation on Export.php code for commenting on code later.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# LaraCSV
+# Alt-LaraCSV
+
+A wrapper for preparing and downloading of CSV file from your model, specifically made for Laravel project.
+
+## Basic usage
+
+###Preparation step:
+
+Laracsv requires two parameter.
+* Your model.
+* Attributes of model.
+
+```php
+$csvExporter = new \Laracsv\Csv\Export();
+
+// Variable $model is your typical Laravel Model
+// Variable $modelAttributes is collection of model fields in an array.
+
+$csvExporter->build($model, $modelAttributes);
+```
+
+###Download step:
+
+To download
+```php
+$csvExporter->download();
+```
--- src/LaraCsv/Export.php
+++ src/LaraCsv/Export.php
@@ -6,43 +6,59 @@ use League\Csv\Writer;
 use SplTempFileObject;
 
 class Export
+
 {
     protected $beforeEachCallback;
     protected $csv;
 
     public function __construct()
+
     {
         $this->csv = Writer::createFromFileObject(new SplTempFileObject());
     }
 
     public function build($collection, array $fields)
+
     {
         $csv = $this->csv;
         $headers = [];
+
         foreach ($fields as $key => $field) {
+
             $headers[] = $field;
 
             if (! is_numeric($key)) {
                 $fields[$key] = $key;
             }
+
         }
 
         $csv->insertOne($headers);
 
         foreach ($collection as $row) {
+
             $beforeEachCallback = $this->beforeEachCallback;
+
             if ($beforeEachCallback) {
+
                 $return = $beforeEachCallback($row);
+
                 if ($return === false) {
                     continue;
                 }
+
             }
 
-            $row = $this->makeAllFieldsVisisble($fields, $row);
+
+            //$row = $row->makeVisible($fields)->toArray();
+            $row = $this->makeAllFieldsVisible($fields, $row);
 
             $csvRow = [];
+
             foreach ($fields as $field) {
+
                 $csvRow[] = array_get($row, $field);
+
             }
 
             $csv->insertOne($csvRow);
@@ -52,23 +68,27 @@ class Export
     }
 
     public function download($filename = null)
+
     {
         $filename = $filename ?: date('Y-m-d_His') . '.csv';
         $this->csv->output($filename);
     }
 
     public function beforeEach(callable $callback)
+
     {
         $this->beforeEachCallback = $callback;
         return $this;
     }
 
     public function getCsv()
+
     {
         return $this->csv;
     }
 
     private function makeAllFieldsVisible(array $fields, $row)
+
     {
         $row = $row->makeVisible($fields)->toArray();
         return $row;
