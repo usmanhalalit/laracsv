d9f1beab31a102fef1ae45066b8cad4e2cdab16f
Muhammad Usman <mdusyl@yahoo.com>
2017-04-01 19:01:11 +0600

    Create test tables

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,7 @@
 <?php namespace Laracsv;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
 use PHPUnit\Framework\TestCase as PhpunitTestCase;
 
 class TestCase extends PhpunitTestCase
@@ -17,17 +18,35 @@ class TestCase extends PhpunitTestCase
         $capsule->setAsGlobal();
         $capsule->bootEloquent();
 
-        Capsule::statement(
-            "CREATE TABLE sample(
-               t_key             TEXT     NOT NULL,
-               t_value           TEXT    NOT NULL
-            );"
-        );
-
+        $this->createTables(Capsule::schema());
     }
 
     public function tearDown()
     {
 
+    }
+
+    private function createTables($schema)
+    {
+        $schema->create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100);
+            $table->decimal('price', 10, 2);
+            $table->decimal('original_price', 10, 2)->nullable();
+            $table->timestamps();
+        });
+
+        $schema->create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 40);
+            $table->timestamps();
+        });
+
+        $schema->create('categories_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->integer('product_id');
+            $table->timestamps();
+        });
     }
 }
