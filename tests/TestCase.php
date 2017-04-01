09f3e57fa8acb4b270f82ceb715daf16f4184c14
Muhammad Usman <mdusyl@yahoo.com>
2017-04-01 17:19:59 +0600

    Use PHPUnit 6

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,12 +1,29 @@
 <?php namespace Laracsv;
 
-use PHPUnit_Framework_TestCase;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use PHPUnit\Framework\TestCase as PhpunitTestCase;
 
-class TestCase extends PHPUnit_Framework_TestCase {
+class TestCase extends PhpunitTestCase
+{
 
     public function setUp()
     {
-		
+        $capsule = new Capsule;
+
+        $capsule->addConnection(array(
+            'driver'  => 'sqlite',
+            'database'  => ':memory:',
+        ));
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+
+        Capsule::statement(
+            "CREATE TABLE sample(
+               t_key             TEXT     NOT NULL,
+               t_value           TEXT    NOT NULL
+            );"
+        );
+
     }
 
     public function tearDown()
