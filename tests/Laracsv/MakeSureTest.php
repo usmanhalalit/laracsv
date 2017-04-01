a078ae5c354a572cd3e5953d8b41b1d5708de904
Muhammad Usman <mdusyl@yahoo.com>
2017-04-01 16:03:15 +0600

    Add test skeleton

---
+++ tests/Laracsv/MakeSureTest.php
@@ -0,0 +1,9 @@
+<?php namespace Laracsv;
+
+class MakeSureTest extends TestCase
+{
+    public function testToMakeSure()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php namespace Laracsv;
+
+use PHPUnit_Framework_TestCase;
+
+class TestCase extends PHPUnit_Framework_TestCase {
+
+    public function setUp()
+    {
+		
+    }
+
+    public function tearDown()
+    {
+
+    }
+}
