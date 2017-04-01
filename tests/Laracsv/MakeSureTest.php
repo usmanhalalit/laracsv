1a16afa65fa1f16de6db4700ace9b9d8a1c11d87
Muhammad Usman <mdusyl@yahoo.com>
2017-04-01 19:27:22 +0600

    Basic first test

--- tests/Laracsv/MakeSureTest.php
+++ tests/Laracsv/MakeSureTest.php
@@ -1,9 +1,26 @@
 <?php namespace Laracsv;
 
+use Laracsv\Export;
+use Laracsv\Models\Product;
+
 class MakeSureTest extends TestCase
 {
     public function testToMakeSure()
     {
         $this->assertTrue(true);
+    }
+
+    public function testBasicCsv()
+    {
+        $products = Product::limit(10)->get();
+
+        $fields = ['id', 'title', 'price', 'original_price',];
+
+        $csvExporter = new Export();
+        $csvExporter->build($products, $fields);
+        $csv = (string) $csvExporter->getCsv();
+        $firstLine = explode(PHP_EOL, $csv)[0];
+        $this->assertEquals("id,title,price,original_price", $firstLine);
+
     }
 }
